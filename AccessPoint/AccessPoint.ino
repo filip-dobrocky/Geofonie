045bc49c19f195a8f4eb0a13f4b7a7c9442b5956
Filip Dobrocky <filip.dobrocky@gmail.com>
2024-10-04 13:43:05 +0200

    Wifi init

--- AccessPoint/AccessPoint.ino
+++ AccessPoint/AccessPoint.ino
@@ -1,6 +1,15 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
 
+const char* ssid     = "TrychtyrLOM";
+const char* password = "LomLomLom";
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("\n[*] Creating AP");
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid, password);
+  Serial.print("[+] AP Created with IP Gateway ");
+  Serial.println(WiFi.softAPIP());
 }
 
 void loop() {
